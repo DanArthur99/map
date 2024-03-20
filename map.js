9604fb7ddd8e92d6fd4029bbfcdbd549233c3ac8
Dan Arthur <danielarthur99@hotmail.co.uk>
2024-03-20 23:43:21 +0000

    feat: mapping

--- map.js
+++ map.js
@@ -5,16 +5,34 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5];
 
+let results = [];
+
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 // Using map()
+const multByTwo = function (num) {
+  return num * 2;
+}
+const mapResults = nums.map(multByTwo);
 
+console.log(mapResults);
 
 // Simplified w/ map()
+
+const mapResults2 = nums.map(function (num) { return num * 2 });
+console.log(mapResults2);
+
 
 
 // Simplfied w/ map() + arrow function
 
+const mapResults3 = nums.map(num => num * 2 );
+console.log(mapResults3);
 
 // With objects:
 const students = [
@@ -37,3 +55,23 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log(studentsWithIds);
+
+const studentNameIdObject = students.map(student => ({id: student.id, name: student.name}));
+console.log(studentNameIdObject);
+
+
+
+const addAgeElement = () => {
+  let ages = [32, 26, 38];
+  let newStudents = students;
+  let x = 0;
+  for (let i=0; i < newStudents.length; i++) {
+    newStudents[i].ages = ages[i];
+  }
+  return newStudents
+}
+
+console.log(addAgeElement());
